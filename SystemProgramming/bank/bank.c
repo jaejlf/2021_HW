4f5585b276a86f61524f0ff7b11491c7694894aa
jaejeong <78673570+jaejlf@users.noreply.github.com>
2021-07-30 13:20:57 +0900

    modify indentation

--- SystemProgramming/bank/bank.c
+++ SystemProgramming/bank/bank.c
@@ -1,7 +1,8 @@
 #include "bank.h"
 #define START_ACC 202100001
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
 	struct account acc;
 	FILE *fp;
@@ -11,25 +12,28 @@ int main(int argc, char *argv[]){
 	int money;
 	long pos;
 
-	if(argc != 2){
+	if (argc != 2)
+	{
 		printf("file name error ! (파일 이름이 잘못되었습니다.)\n");
 		printf("(ex) ./bank filename\n");
 		exit(1);
 	}
 
-	if((fp = fopen(argv[1], "rb+"))==NULL){
+	if ((fp = fopen(argv[1], "rb+")) == NULL)
+	{
 		fp = fopen(argv[1], "ab+");
 		fclose(fp);
 		fp = fopen(argv[1], "rb+");
 	}
 
-	while(1){
+	while (1)
+	{
 
 		//menu print
 		printf("----Menu----\n");
 		printf("1. 계좌생성\n");
 		printf("2. 입금\n");
-	 	printf("3. 출금\n"); 
+		printf("3. 출금\n");
 		printf("4. 잔액조회\n");
 		printf("5. 프로그램 종료\n");
 		printf("------------\n");
@@ -37,30 +41,38 @@ int main(int argc, char *argv[]){
 		scanf("%d", &menu);
 
 		//메뉴 입력 오류
-		if(menu <= 0 || menu > 5){
+		if (menu <= 0 || menu > 5)
+		{
 			printf("잘못된 메뉴를 입력하였습니다. (메뉴 1 ~ 5)\n");
 			printf("\n");
 		}
 
 		//5번 :: 종료
-		if(menu == 5) return 0;
+		if (menu == 5)
+			return 0;
 
 		//1번 :: 계좌 생성
-		else if(menu == 1){
+		else if (menu == 1)
+		{
 
 			printf("계좌 생성 메뉴 - 계좌번호, 이름, 초기 입금액을 띄어쓰기로 구분하여 입력해주시기 바랍니다.\n");
 			scanf("%d %s %d", &id, name, &money);
-		
+
 			//잘못된 계좌번호 입력
-			if(id < 202100001 || id > 999999999){
+			if (id < 202100001 || id > 999999999)
+			{
 				printf("잘못된 계좌 번호를 입력하였습니다. (유효 계좌번호 202100001 ~ 999999999)\n");
 			}
-			else{
-				pos = (id - START_ACC)*sizeof(acc);
-				fseek(fp, pos, SEEK_SET);			
-				if(fread(&acc, sizeof(acc), 1, fp) > 0 && (acc.id != 0)){
+			else
+			{
+				pos = (id - START_ACC) * sizeof(acc);
+				fseek(fp, pos, SEEK_SET);
+				if (fread(&acc, sizeof(acc), 1, fp) > 0 && (acc.id != 0))
+				{
 					printf("이미 존재하는 계좌번호입니다.\n");
-				} else{
+				}
+				else
+				{
 					fseek(fp, pos, SEEK_SET);
 					acc.id = id;
 					strcpy(acc.name, name);
@@ -68,104 +80,117 @@ int main(int argc, char *argv[]){
 					fwrite(&acc, sizeof(acc), 1, fp);
 				}
 			}
-			printf("\n"); 
-		} 
+			printf("\n");
+		}
 
 		//2번 :: 입금
-		else if(menu == 2){
+		else if (menu == 2)
+		{
 
 			printf("입금 메뉴\n");
 			printf("계좌 번호를 입력하세요 : ");
 			scanf("%d", &id);
 
 			//잘못된 계좌번호 입력
-			if(id < 202100001 || id > 999999999){
+			if (id < 202100001 || id > 999999999)
+			{
 				printf("잘못된 계좌 번호를 입력하였습니다. (유효 계좌번호 202100001 ~ 999999999)\n");
 			}
-			else{
-				pos = (id - START_ACC)*sizeof(acc);
+			else
+			{
+				pos = (id - START_ACC) * sizeof(acc);
 				fseek(fp, pos, SEEK_SET);
-				if(fread(&acc, sizeof(acc), 1, fp) > 0 && (acc.id != 0)){
+				if (fread(&acc, sizeof(acc), 1, fp) > 0 && (acc.id != 0))
+				{
 					printf("입금할 금액을 입력하세요 : ");
 					scanf("%d", &money);
 
 					fseek(fp, pos, SEEK_SET);
 					acc.money += money;
 					fwrite(&acc, sizeof(acc), 1, fp);
-
-				} else{
+				}
+				else
+				{
 					printf("NO RECORD !\n");
 				}
 			}
 			printf("\n");
-
 		}
 
 		//3번 :: 출금
-		else if(menu == 3){
+		else if (menu == 3)
+		{
 
 			printf("출금 메뉴\n");
 			printf("계좌 번호를 입력하세요 : ");
 			scanf("%d", &id);
 
 			//잘못된 계좌번호 입력
-			if(id < 202100001 || id > 999999999){
+			if (id < 202100001 || id > 999999999)
+			{
 				printf("잘못된 계좌 번호를 입력하였습니다. (유효 계좌번호 202100001 ~ 999999999)\n");
 			}
-			else{
-				pos = (id - START_ACC)*sizeof(acc);
+			else
+			{
+				pos = (id - START_ACC) * sizeof(acc);
 				fseek(fp, pos, SEEK_SET);
-				if(fread(&acc, sizeof(acc), 1, fp) > 0 && (acc.id != 0)){
+				if (fread(&acc, sizeof(acc), 1, fp) > 0 && (acc.id != 0))
+				{
 					printf("출금할 금액을 입력하세요 : ");
 					scanf("%d", &money);
-		
+
 					fseek(fp, pos, SEEK_SET);
-					if(acc.money < money) printf("잔액 부족 !\n");
-					else{
+					if (acc.money < money)
+						printf("잔액 부족 !\n");
+					else
+					{
 						acc.money -= money;
 						fwrite(&acc, sizeof(acc), 1, fp);
 					}
-					
-				} else{
+				}
+				else
+				{
 					printf("NO RECORD !\n");
 				}
 			}
 			printf("\n");
-
 		}
 
 		//4번 :: 잔액조회
-		else if(menu == 4){
+		else if (menu == 4)
+		{
 
 			printf("잔액조회 메뉴\n");
 			printf("계좌 번호를 입력하세요 : ");
 			scanf("%d", &id);
 
 			//잘못된 계좌번호 입력
-			if(id < 202100001 || id > 999999999){
+			if (id < 202100001 || id > 999999999)
+			{
 				printf("잘못된 계좌 번호를 입력하였습니다. (유효 계좌번호 202100001 ~ 999999999)\n");
 			}
-			else{
-				pos = (id - START_ACC)*sizeof(acc);
+			else
+			{
+				pos = (id - START_ACC) * sizeof(acc);
 				fseek(fp, pos, SEEK_SET);
-				if(fread(&acc, sizeof(acc), 1, fp) > 0 && (acc.id != 0)){
+				if (fread(&acc, sizeof(acc), 1, fp) > 0 && (acc.id != 0))
+				{
 					fseek(fp, pos, SEEK_SET);
 					printf("* 계좌번호 : %d\n", acc.id);
 					printf("* 이름 : %s\n", acc.name);
 					printf("* 잔액 : %d\n", acc.money);
-
-				} else{
+				}
+				else
+				{
 					printf("NO RECORD !\n");
 				}
 			}
 			printf("\n");
-
 		}
 
 		getchar();
 	} //while-end
-	
+
 	fclose(fp);
 	return 0;
-	
-}	
+}
--- SystemProgramming/ls_cmd/list.c
+++ SystemProgramming/ls_cmd/list.c
@@ -2,9 +2,10 @@
 
 char type(mode_t);
 char *perm(mode_t);
-void printStat(char*, char*, struct stat*, char*, int);
+void printStat(char *, char *, struct stat *, char *, int);
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
 
 	DIR *dp;
 	char *dir;
@@ -17,85 +18,105 @@ int main(int argc, char **argv){
 	char option[BUFSIZ];
 
 	//command input
-	if (argc == 1){
+	if (argc == 1)
+	{
 		dir = ".";
 	}
 
-	else if(argc == 2){
+	else if (argc == 2)
+	{
 
-		if (*argv[1] == '-'){ //argv[1] is option
+		if (*argv[1] == '-')
+		{ //argv[1] is option
 			dir = ".";
 
 			i = 0;
-			while(1){
-				if(*(argv[1] + i) == NULL) break;
-				else{
+			while (1)
+			{
+				if (*(argv[1] + i) == NULL)
+					break;
+				else
+				{
 					option[i] = *(argv[1] + i);
 					op++;
 				}
 				i++;
 			}
 		}
-		else{	
+		else
+		{
 			dir = argv[1];
 		}
 	}
-	else{ //argc == 3
-		if (*argv[1] == '-'){ //argv[1] is option
+	else
+	{ //argc == 3
+		if (*argv[1] == '-')
+		{ //argv[1] is option
 			dir = argv[2];
 
 			i = 0;
-			while(1){
-				if(*(argv[1] + i) == NULL) break;
-				else{
+			while (1)
+			{
+				if (*(argv[1] + i) == NULL)
+					break;
+				else
+				{
 					option[i] = *(argv[1] + i);
 					op++;
 				}
 				i++;
 			}
 		}
-		else{ //argv[2] is option
+		else
+		{ //argv[2] is option
 			dir = argv[1];
 
 			i = 0;
-			while(1){
-				if(*(argv[2] + i) == NULL) break;
-				else{
+			while (1)
+			{
+				if (*(argv[2] + i) == NULL)
+					break;
+				else
+				{
 					option[i] = *(argv[2] + i);
 					op++;
 				}
 				i++;
 			}
 		}
-	}	
-	
+	}
+
 	//main
-	if((dp = opendir(dir)) == NULL) {
+	if ((dp = opendir(dir)) == NULL)
+	{
 		perror(dir);
 	}
-	
-	while((d = readdir(dp)) != NULL){
 
-		if (!strncmp(d->d_name, ".", 1)) continue;
+	while ((d = readdir(dp)) != NULL)
+	{
+
+		if (!strncmp(d->d_name, ".", 1))
+			continue;
 
 		sprintf(path, "%s/%s", dir, d->d_name);
-		if(lstat(path, &st) < 0){
+		if (lstat(path, &st) < 0)
+		{
 			perror(path);
 		}
-		
-		if(op == 0){
-			printf("%s", d->d_name);
 
-		} else{ //ls with option : s, i, l, F, o
+		if (op == 0)
+		{
+			printf("%s", d->d_name);
+		}
+		else
+		{ //ls with option : s, i, l, F, o
 			printStat(path, d->d_name, &st, option, op);
 		}
 
-		putchar('\n'); 
+		putchar('\n');
 
 	} // while-end;
 
 	closedir(dp);
 	exit(0);
-
 }
-
